29627fd06c1ac3c38d82a71b50b80dbf6fd531b0
muneeb0305 <muneebahmed0305@gmail.com>
2024-10-24 00:01:27 +0500

    Updates

---
+++ static/js/732.13f669fc.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[732],{7732:(e,r,t)=>{t.r(r),t.d(r,{default:()=>o});t(5043);var n=t(579);function o(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h1",{children:"About"})})}}}]);
+//# sourceMappingURL=732.13f669fc.chunk.js.map
